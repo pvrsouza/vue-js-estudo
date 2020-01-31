33e47a695140cc30398a5348d954919e9877daa7
Pablo Souza <pvrsouza@gmail.com>
2020-01-31 06:53:37 -0300

    primeiro commit

---
+++ componentes-avancados/citacoes/src/components/Citacao.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="citacao">{{citacao}}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    citacao: {
+      type: String
+    }
+  }
+};
+</script>
+
+<style scoped>
+.citacao {
+  border: 1px solid #ddd;
+  background-color: rgba(0, 0, 0, 0.1);
+  padding: 25px;
+  max-width: 450px;
+}
+</style>
